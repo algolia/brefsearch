dcff7143c22aca0a4b36d575a9575a8b809be8ec
Tim Carry <tim@pixelastic.com>
2025-02-13 12:16:17 +0100

    Add update to Algolia

---
+++ scripts/deploy-data-algolia.js
@@ -0,0 +1,36 @@
+import indexing from 'algolia-indexing';
+import { absolute, glob, readJson } from 'firost';
+import { pMap } from 'golgoth';
+import config from '../lib/config.js';
+
+(async function () {
+  const credentials = {
+    appId: config.appId,
+    indexName: config.indexName,
+    apiKey: config.adminApiKey,
+  };
+  const settings = {
+    searchableAttributes: ['line.content', 'episode.name'],
+    attributesForFaceting: [
+      'searchable(episode.name)',
+      'episode.index',
+      'episode.videoId',
+    ],
+    customRanking: ['asc(episode.index)', 'asc(line.index)'],
+    attributeForDistinct: 'episode.videoId',
+    distinct: true,
+  };
+
+  indexing.verbose();
+  indexing.config({
+    batchMaxSize: 100,
+  });
+
+  const files = await glob('**/*.json', {
+    cwd: absolute('<packageRoot>/data/records'),
+  });
+  const data = await pMap(files, async (filepath) => {
+    return await readJson(filepath);
+  });
+  await indexing.fullAtomic(credentials, data, settings);
+})();
