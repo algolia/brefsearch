30112dbbeb2616db3c904276c86365643f40a882
Tim Carry <tim@pixelastic.com>
2025-02-13 16:20:50 +0100

    test next

---
+++ modules/website/src/app/page.js
@@ -0,0 +1,12 @@
+import SearchComponent from './components/SearchComponent';
+
+/**
+ *
+ */
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white">
+      <SearchComponent />
+    </main>
+  );
+}
