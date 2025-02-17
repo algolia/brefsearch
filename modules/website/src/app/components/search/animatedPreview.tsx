1c093e4d92c6e8c32bbee03a6d0f17b0d227e951
LukyVj <lucas.bonomi@gmail.com>
2025-02-17 19:02:36 +0100

    fix(Misc): fix vercel build

--- modules/website/src/app/components/search/animatedPreview.tsx
+++ modules/website/src/app/components/search/animatedPreview.tsx
@@ -4,12 +4,9 @@ import Image from 'next/image';
 
 // Types
 import type { Hit as AlgoliaHit } from 'instantsearch.js/es/types';
+import { BrefHit } from '@/app/types';
 
-const AnimatedPreview = ({
- hit,
-}: {
-  hit: AlgoliaHit<BrefHit>;
-}) => {
+const AnimatedPreview = ({ hit }: { hit: AlgoliaHit<BrefHit> }) => {
   /**
    * How it works:
    * On first load, an image of a transparent gif is displayed but the actual
@@ -25,12 +22,13 @@ const AnimatedPreview = ({
    * revert to the invisible gif (so it doesn't loop in the background for
    * nothing).
    **/
-  const transparentGif = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+  const transparentGif =
+    'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
   const gifUrl = hit.thumbnail.gifUrl;
   const alt = hit.episode.name;
 
   // On first load, fetch the gif blob and save it as a ref
-  const gifBlob = useRef(null);
+  const gifBlob = useRef<Blob | null>(null);
   const isFirstLoad = useRef(true);
   useEffect(() => {
     // Only load this once
@@ -39,14 +37,13 @@ const AnimatedPreview = ({
 
     // Fetch the blob, save it as a ref, and update the Image src
     async function fetchGifData() {
-      const response = await fetch(gifUrl)
+      const response = await fetch(gifUrl);
       const blob = await response.blob();
-      gifBlob.current = blob
+      gifBlob.current = blob;
       setGifSrc(URL.createObjectURL(blob));
     }
     fetchGifData();
   });
-
 
   // Swap the src between the transparent gif and the actual gif when mouse
   // enters/leaves.
@@ -56,11 +53,11 @@ const AnimatedPreview = ({
     if (!gifBlob.current) return;
     URL.revokeObjectURL(gifSrc);
     setGifSrc(URL.createObjectURL(gifBlob.current));
-  }
+  };
   const onMouseLeave = () => {
     if (!gifBlob.current) return;
     setGifSrc(transparentGif);
-  }
+  };
 
   return (
     <Image
@@ -68,13 +65,13 @@ const AnimatedPreview = ({
       alt={alt}
       fill
       sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-      className={cx("object-cover absolute z-10 invisible group-hover:visible")}
+      className={cx('object-cover absolute z-10 invisible group-hover:visible')}
       priority={false}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
       unoptimized
     />
-  )
+  );
 };
 
 export default AnimatedPreview;
