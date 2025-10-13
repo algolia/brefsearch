425c1dedfea7c908e75f28857f309af2cfcc12f6
Tim Carry <tim@pixelastic.com>
2025-10-13 21:18:14 +0200

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 import config from 'aberlaas/configs/eslint';
 import react from 'eslint-plugin-react';
 import globals from 'globals';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
 
 export default [
   ...config,
@@ -22,6 +24,40 @@ export default [
     rules: {
       'react/jsx-uses-react': 'error',
       'react/jsx-uses-vars': 'error',
+    },
+  },
+  {
+    files: ['**/modules/website/**/*.{ts,tsx}'],
+    plugins: {
+      react,
+      '@typescript-eslint': tseslint,
+    },
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './modules/website/tsconfig.json',
+        },
+      },
+    },
+    rules: {
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
 ];
