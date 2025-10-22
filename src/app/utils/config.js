5280f745578e920baa876a2f22898ff23edf5de3
Tim Carry <tim@pixelastic.com>
2025-10-22 14:16:19 +0200

    Use env variables

--- src/app/utils/config.js
+++ src/app/utils/config.js
@@ -1,6 +1,10 @@
 export const config = {
-  appId: 'O3F8QXYK6R',
-  searchApiKey: '6a47b9b62c58a7a4cd2338f095630b15',
-  indexName: 'brefsearch',
-  indexNamePopularity: 'brefsearch_popularity',
+  appId: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || 'O3F8QXYK6R',
+  searchApiKey:
+    process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY ||
+    '6a47b9b62c58a7a4cd2338f095630b15',
+  indexName: process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME || 'brefsearch',
+  indexNamePopularity:
+    process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME_POPULARITY ||
+    'brefsearch_popularity',
 };
